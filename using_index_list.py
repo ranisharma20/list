5623f58bb2f64bb49331e6639ca10898089a145c
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-06-17 11:44:34 +0530

    Add files via upload

---
+++ using_index_list.py
@@ -0,0 +1,10 @@
+matrix = [[0, 1, 2, 3, 4],
+          [0, 1, 2, 3, 4],
+          [0, 1, 2, 3, 4],
+          [0, 1, 2, 3, 4],
+          [0, 1, 2, 3, 4]]
+print(matrix[1][3])
+print(matrix[1][2])
+print(matrix[1][4])
+print(matrix[1][0])
+print(matrix[1][1])
